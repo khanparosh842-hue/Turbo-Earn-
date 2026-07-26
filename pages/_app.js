215ab576b6d97c41c710ba6876ebae1537ce4fe3
khanparosh842-hue <khanparosh842@gmail.com>
2026-07-26 21:43:06 +0600

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+// ================================================
+// FILE PATH (GitHub এ ঠিক এই path/নামে বসাবেন): pages/_app.js
+// ================================================
+
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
